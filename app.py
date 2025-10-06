82bff33d08681007e5114b21e5ffbfddfb3c7dcb
Praveen <muthupraveens@gmail.com>
2025-10-07 01:31:10 +0800

    Change server name to 0.0.0.0 for cloud deployment

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ with gr.Blocks(title="Face Detection & Emotion Classification") as demo:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    demo.launch(server_name="localhost", server_port=port, debug=True)
+    demo.launch(server_name="0.0.0.0", server_port=port)
